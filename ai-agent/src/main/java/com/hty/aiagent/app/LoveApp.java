973d95d027e48e437ee9be5392d4fde1dc8a68d1
Hty <15161656368@163.com>
2026-05-13 21:27:15 +0800

    RAG知识库基础

--- ai-agent/src/main/java/com/hty/aiagent/app/LoveApp.java
+++ ai-agent/src/main/java/com/hty/aiagent/app/LoveApp.java
@@ -3,13 +3,16 @@ package com.hty.aiagent.app;
 import com.hty.aiagent.advisor.MyLoggerAdvisor;
 import com.hty.aiagent.advisor.ReReadingAdvisor;
 import com.hty.aiagent.chatmemory.FileBasedChatMemory;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -89,6 +92,28 @@ public class LoveApp {
                 .entity(LoveReport.class);
         log.info("loveReport: {}", loveReport);
         return loveReport;
+    }
+
+    //AI 恋爱知识库问答功能
+
+    @Resource
+    private VectorStore loveAppVectorStore;
+
+    public String doChatWithRag(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))//对话记忆
+                //开启日志
+                .advisors(new MyLoggerAdvisor())
+                //应用RAG知识库问答
+                .advisors(new QuestionAnswerAdvisor(loveAppVectorStore))
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
     }
 
 
