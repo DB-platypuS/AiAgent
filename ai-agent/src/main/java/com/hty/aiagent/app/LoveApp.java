9b3fec8154d29c50eba6d50cc304afad8aa5ef50
Hty <15161656368@163.com>
2026-05-28 17:01:56 +0800

    AI服务化

--- ai-agent/src/main/java/com/hty/aiagent/app/LoveApp.java
+++ ai-agent/src/main/java/com/hty/aiagent/app/LoveApp.java
@@ -19,6 +19,7 @@ import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -114,7 +115,7 @@ public class LoveApp {
     private QueryRewriter queryRewriter;
 
     /**
-     * 和RAG知识库进行对话
+     * 和 RAG 知识库进行对话
      * @param message
      * @param chatId
      * @return
@@ -173,6 +174,22 @@ public class LoveApp {
         String content = response.getResult().getOutput().getText();
         log.info("content: {}", content);
         return content;
+    }
+
+    /**
+     * AI基础对话（支持多轮对话记忆，SSE 流式传输）
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public Flux<String> doChatByStream(String message, String chatId) {
+        return chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .stream()
+                .content();
     }
 
     //AI 调用 MCP 能力
